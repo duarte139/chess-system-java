3caeb784a9f5c06a98e7aa8c8654d5ebabd8f037
duarte139 <34600086+duarte139@users.noreply.github.com>
2019-03-06 21:40:05 -0300

    Starting to implement Board and Piece

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,14 +1,12 @@
 package aplication;
 
-import boardgame.Position;
+import boardgame.Board;
 
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Position pos = new Position(3, 5);
-		System.out.println(pos);
-
+		Board board = new Board(8, 8);
 	}
 
 }
